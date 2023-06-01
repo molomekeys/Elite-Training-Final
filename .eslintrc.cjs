94932c815c05d6abc41cfaf2af434e716cb58330
Ouzar Merouane <ouzar.merouane@gmail.com>
2023-06-01 13:20:35 -0400

    j'ai désactiver dans es-lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,7 +30,8 @@ const config = {
     ],
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-misused-promises": "off", // Disable the rule here
-  "@typescript-eslint/no-floating-promises":'off'
+  "@typescript-eslint/no-floating-promises":'off',
+"  @typescript-eslint/restrict-plus-operands":"off"
   },
 };
 

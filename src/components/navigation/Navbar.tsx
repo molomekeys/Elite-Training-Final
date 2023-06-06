bc6f57f97feac72adef3358fb5221bd0ee1d13d2
Ouzar Merouane <ouzar.merouane@gmail.com>
2023-06-06 18:24:57 -0400

    normalement la navbar devrais fonctionner

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import Link  from "next/link"
 import {motion,AnimatePresence, delay} from 'framer-motion'
 import {useState} from 'react'
 const Navbar = () => {
@@ -9,7 +9,7 @@ const Navbar = () => {
     className='w-full flex absolute text-center relative z-10 p-4   shadow-sm lg:flex-row gap-4   justify-between w-screen  bg-white    items-center  lg:px-20'>
   <motion.div  animate={{opacity:1,x:0}} initial={{opacity:1,x:'-100%'}} transition={{duration:0.75}}
   className="w-full  h-full text-left">
-   <Link to='/'>
+   <Link href='/'>
     <p className='font-semibold   text-emerald-600 text-3xl  inline  cursor-pointer z-20 relative' onClick={()=> window.scrollTo(0,0)}>Food Easy</p>
     </Link>
     </motion.div>
@@ -35,12 +35,12 @@ className=" bg-slate-700 bg-opacity-5 w-full  relative h-full z-50">
 >
     <ul className=" relative w-full h-full z-50 flex self-start  text-lg flex-col bg-slate-100 text-slate-800 w-screen h-screen  items-start  p-10 gap-4  font-semibold">
    
-   <Link to="/meal">
+   <Link href="/meal">
         <li className=" hover:bg-white w-screen  self-start flex py-2 px-1" onClick={()=>{   window.scrollTo(0,0)
           setLayoutMobile(false) 
          } }>All meals</li>
         </Link>
-        <Link to='/recipe'>
+        <Link href='/recipe'>
       <li className="  hover:bg-white w-screen  self-start flex py-2 px-1 "  onClick={()=>setLayoutMobile(false) } >Discover a meal</li>
       </Link>
        
@@ -54,12 +54,12 @@ className=" bg-slate-700 bg-opacity-5 w-full  relative h-full z-50">
     animate={{opacity:1,y:0}} initial={{opacity:0,y:'-100%'}} transition={{duration:0.5,delay:0.25}} 
     exit={{x:'100%'}}
     className="w-full flex  gap-10 items-end justify-end items-center">
-    <Link to='/meal'>
+    <Link href='/meal'>
   <motion.button className='p-2 px-4 hidden lg:block lg:px-12 cursor-pointer bg-emerald-600 text-slate-100 border-2 font-semibold border-emerald-600 text-base text-slate-700 text-md rounded-lg text-center  '>Discover our recipes</motion.button>
 
   </Link>
   
-  <Link to='/recipe'>
+  <Link href='/recipe'>
   <motion.button className='p-2 hidden lg:block  px-4 lg:px-12 cursor-pointer  bg-white font-semibold text-slate-800 text-md rounded- text-base  border-slate-800 rounded-lg lg text-center  border-2   shadow-sm ' onClick={()=> window.scrollTo(0,0)}>Explore a new flavor</motion.button>
 
   </Link>

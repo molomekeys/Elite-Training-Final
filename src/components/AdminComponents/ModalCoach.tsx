87fc1f0e22688d90765091110b5271da7e235cef
Ouzar Merouane <ouzar.merouane@gmail.com>
2023-06-07 12:29:15 -0400

    vercel probleme de deployement

--- src/components/AdminComponents/ModalCoach.tsx
+++ src/components/AdminComponents/ModalCoach.tsx
@@ -9,7 +9,7 @@ import {
    
     ModalCloseButton,Button
   } from '@chakra-ui/react'
-  import {useDisclosure} from '@chakra-ui/react'
+
   import {useRef, useState} from 'react'
 
 import * as z from 'zod';
